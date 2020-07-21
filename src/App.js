2d3c77ac5742a3ccfff6e063a430f7ffd68d1713
metalsorrow <marckfuentes2009@hotmail.com>
2020-07-21 02:48:09 -0400

    Update menu-items, add react router to nav base items in shop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from "react-router-dom";
+
 import HomePage from './pages/homepage.component'
+
+
+
+const HastPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component ={HomePage}/>
+        <Route path='/hats' component ={HastPage}/>
+        {/* <HomePage /> */}
+      </Switch>
     </div>
   );
 }
